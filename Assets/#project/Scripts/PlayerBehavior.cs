7f15cd78eb31a58248cb1723f3a4f9021080e1a7
Marie <fzgaoui@hotmail.fr>
2021-11-12 16:21:21 +0100

    avancement fix rotation pivot du player + je dois encore fix la camera

--- Assets/#project/Scripts/PlayerBehavior.cs
+++ Assets/#project/Scripts/PlayerBehavior.cs
@@ -17,7 +17,9 @@ public class PlayerBehavior : MonoBehaviour
     public Transform rayFrontUnder;
     public float durationRotation = 2f;
     public List<Transform> rays = new List<Transform>(4);
-    private bool isRotating = false;
+    public bool isRotating = false;
+
+    public Transform pivot;
 
 
     // Start is called before the first frame update
@@ -31,16 +33,10 @@ public class PlayerBehavior : MonoBehaviour
     public void Movement(InputAction.CallbackContext context){ //cette méthode lie à l'inpput player pour le déplacement
         moveVector = context.ReadValue<Vector2>();
         // Debug.Log(moveVector);
+        
     }
     void Update()
     {
-        
-        Vector3 camP = new Vector3();
-        camP = transform.position - cam.position;
-        Vector3 cameraPlayer2D = new Vector3(camP.x, 0, camP.z);
-        cameraPlayer2D = cameraPlayer2D.normalized;
-        Vector3 movement =  Vector3.zero;
-
         //groupe de 4 raycasts
         Vector3 direction = transform.up * -1;
         List<RaycastHit> raycasts = new List<RaycastHit>();
@@ -50,6 +46,27 @@ public class PlayerBehavior : MonoBehaviour
             Physics.Raycast(rays[i].position, direction,  out hit, hitRange);
             raycasts.Add(hit);
         }
+
+        Vector3 camP = new Vector3();
+        camP = transform.position - cam.position;
+        Vector3 cameraPlayer2D = new Vector3(camP.x, 0, camP.z).normalized;
+
+        //récupération de la normal when rayforward hit
+        if(raycasts[0].collider != null && !isRotating){
+            Vector3 result = raycasts[0].normal;
+            // Vector3[] result = platforme.GetComponent<MeshFilter>().mesh.normals;
+            if(result == Vector3.up || result == Vector3.down){
+                cameraPlayer2D = new Vector3(camP.x, 0, camP.z).normalized;
+            }else if(result == Vector3.left || result == Vector3.right){
+                cameraPlayer2D = new Vector3(0, camP.y, camP.z).normalized;
+            }else if(result == Vector3.forward || result == Vector3.back){
+                cameraPlayer2D = new Vector3(camP.x, camP.y, 0).normalized;
+            }           
+            print(result);
+        }
+
+        // cameraPlayer2D = cameraPlayer2D.normalized;
+        Vector3 movement =  Vector3.zero;
 
         //création du raycast vers le bas
         Debug.DrawRay(transform.position, direction * hitRange, Color.green);
@@ -73,7 +90,7 @@ public class PlayerBehavior : MonoBehaviour
             print("rotate down devient true");
             if(!isRotating){
                 StartCoroutine(RotationDown());
-                footRotateDown = true;
+                
 
             }
             // transform.Rotate(90f, 0f, 0f);
@@ -93,18 +110,18 @@ public class PlayerBehavior : MonoBehaviour
         
         
         // limite le movement avant arrière
-        if(raycasts[0].collider == null && !firstTime && moveVector.y > 0 && !footRotateDown){
+        if(raycasts[0].collider == null && !firstTime && moveVector.y > 0 ){
             moveVector.y = 0;      
         }
-        if(raycasts[1].collider == null && !firstTime && moveVector.y < 0){
+        if(raycasts[1].collider == null && !firstTime && moveVector.y < 0 && !isRotating){
             moveVector.y = 0; 
         }    
         
         // limite movement gauche droite
-        if(raycasts[2].collider == null && !firstTime && moveVector.x > 0){
+        if(raycasts[2].collider == null && !firstTime && moveVector.x > 0 && !isRotating){
             moveVector.x = 0;          
         }    
-        if(raycasts[3].collider == null && !firstTime && moveVector.x < 0){
+        if(raycasts[3].collider == null && !firstTime && moveVector.x < 0 && !isRotating){
             moveVector.x = 0;       
         }    
         // règle le mouvement forward backward en fonction  de la camera et le mouvement de gauche droite en fonction du player       
@@ -115,14 +132,12 @@ public class PlayerBehavior : MonoBehaviour
         if(movement!= Vector3.zero ){ // movement.magnitude.zero => on peut faire ça aussi , magnitude c'est la longueur du vecteur
             // permet que le perso regarde vers là où il va, le perso se tourne doucement
             transform.forward = cameraPlayer2D;
+            controller.Move( movement * Time.deltaTime); //le joueur se déplace Move est une méthode du player controller
 
             
         }
-
-        controller.Move( movement * Time.deltaTime); //le joueur se déplace Move est une méthode du player controller
-
         
-
+        
         
     }
 
@@ -131,16 +146,24 @@ public class PlayerBehavior : MonoBehaviour
         float time = 0;
         Quaternion start = transform.rotation;
         Quaternion r = Quaternion.Euler(start.eulerAngles + Vector3.right * 90);
-        float ratio= 0;
-        while (ratio < 1f){
-            time = Time.deltaTime;
-            ratio += time / durationRotation;
-            print(ratio);
+        float ratio = 0;
+
+        while (ratio <1f){
+            time += Time.deltaTime;
+            ratio = time / durationRotation;
+            // print(ratio);
             transform.rotation =  Quaternion.Lerp(start, r, ratio);
+            //transform.Rotate(Vector3.right * speedRotation * Time.deltaTime, Space.Self);
+            //Quaternion angulu = Quaternion.Lerp(start, r, ratio);
+            // transform.Rotate(Vector3.right * 90, Space.Self);
+            
             yield return true;
             // yield return new WaitForEndOfFrame(); c'est pareil que yield return true
         }
+        
         isRotating = false;
 
     }
+
+
 }
