774af573c13435a1f47a579670c33bc960f50656
Marie <fzgaoui@hotmail.fr>
2021-11-16 11:34:47 +0100

    draft rotation

--- Assets/#project/Scripts/PlayerBehavior.cs
+++ Assets/#project/Scripts/PlayerBehavior.cs
@@ -10,48 +10,53 @@ public class PlayerBehavior : MonoBehaviour
     Vector2 moveVector;
     public Transform cam;
     public float hitRange = 0.1f;
-    private bool firstTime;
     public Transform startingPoint;
-    public bool footRotateDown;
+    private bool firstTime;
     public Transform rayFront;
     public Transform rayFrontUnder;
     public float durationRotation = 2f;
     public List<Transform> rays = new List<Transform>(4);
     public bool isRotating = false;
+    public bool moveFurther;
+
 
     public Transform pivot;
-    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         firstTime = true;
-        // footRotateDown = false;
+
     }
 
     // Update is called once per frame
-    public void Movement(InputAction.CallbackContext context){ //cette méthode lie à l'inpput player pour le déplacement
+    public void Movement(InputAction.CallbackContext context)
+    { //cette méthode lie à l'inpput player pour le déplacement
         moveVector = context.ReadValue<Vector2>();
         // Debug.Log(moveVector);
-        
+
     }
     void Update()
     {
         //groupe de 4 raycasts
         Vector3 direction = transform.up * -1;
         List<RaycastHit> raycasts = new List<RaycastHit>();
-        for(int i = 0; i < rays.Count; i++){
+        for (int i = 0; i < rays.Count; i++)
+        {
             Debug.DrawRay(rays[i].position, direction * hitRange, Color.red);
-            RaycastHit hit ;
-            Physics.Raycast(rays[i].position, direction,  out hit, hitRange);
+            RaycastHit hit;
+            Physics.Raycast(rays[i].position, direction, out hit, hitRange);
             raycasts.Add(hit);
         }
 
         //création du raycast vers le bas
-        Debug.DrawRay(transform.position, direction * hitRange, Color.green);
         RaycastHit objectHit;
-        Physics.Raycast(transform.position, direction,  out objectHit, hitRange);
+        Color e = Color.red;
+        if(Physics.Raycast(transform.position, direction, out objectHit, hitRange)){
+            e = Color.green; }
+        Debug.DrawRay(transform.position, direction * hitRange, e);
 
         Vector3 camP = new Vector3();
         camP = transform.position - cam.position;
@@ -75,129 +80,157 @@ public class PlayerBehavior : MonoBehaviour
         // }
 
         // cameraPlayer2D = cameraPlayer2D.normalized;
-        Vector3 movement =  Vector3.zero;
+        Vector3 movement = Vector3.zero;
 
         //création du raycast vers front
         Vector3 front = transform.forward;
-        Debug.DrawRay(rayFront.position, front * (hitRange * 2), Color.blue);
-        RaycastHit rayFrontHit ;
-        Physics.Raycast(rayFront.position, front,  out rayFrontHit, hitRange);
+        RaycastHit rayFrontHit;
+        Color c = Color.red;
+        if (Physics.Raycast(rayFront.position, front, out rayFrontHit, hitRange))
+        { c = Color.green; }
+        Debug.DrawRay(rayFront.position, front * (hitRange * 2), c);
 
         //création du raycast vers front en bas
         Vector3 under = transform.forward;
-        Debug.DrawRay(rayFrontUnder.position, front * (hitRange * 2), Color.blue);
-        RaycastHit rayFrontUndertHit ;
-        Physics.Raycast(rayFrontUnder.position, front,  out rayFrontUndertHit, hitRange);
+        RaycastHit rayFrontUndertHit;
+        Color d = Color.red;
+        if(Physics.Raycast(rayFrontUnder.position, front, out rayFrontUndertHit, hitRange))
+        { d=Color.green;}
+        Debug.DrawRay(rayFrontUnder.position, front * (hitRange * 2), d);
+
         
+
         //condition pour activer la rotationvers le bas
-        if(rayFrontHit.collider == null && rayFrontUndertHit.collider != null && raycasts[0].collider == null && objectHit.collider == null){
+        if (rayFrontHit.collider == null && rayFrontUndertHit.collider != null )
+        {
             print("rotation");
-            // footRotateDown = true;
-            if(!isRotating){
+            moveFurther = true;
+            if (!isRotating && objectHit.collider == null)
+            {
                 StartCoroutine(RotationDown(rayFrontUndertHit));
-                
 
             }
             // transform.Rotate(90f, 0f, 0f);
-            
+        }else{
+            moveFurther = false;
         }
 
         //vérification firstTime
-        if(firstTime){
-            if(moveVector.y != 0 || moveVector.x != 0){
+        if (firstTime)
+        {
+            if (moveVector.y != 0)
+            {
                 movement = transform.forward * moveVector.y;
                 // movement =  new Vector3(moveVector.x, 0, moveVector.y) * movementSpeed;
             }
-            
-            if (transform.position != startingPoint.position){ //met le firstime à false
-                firstTime =false;
+
+            if (transform.position != startingPoint.position)
+            { //met le firstime à false
+                firstTime = false;
             }
         }
 
-        
-        
-        if(!isRotating && footRotateDown){
-            moveVector.y = 0;         
+
+
+        if (isRotating)
+        {
+            moveVector.y = 0;
         }
         // limite le movement avant arrière
-        if(raycasts[0].collider == null && !firstTime && moveVector.y > 0 && !isRotating){
+        if (raycasts[0].collider == null && !firstTime && moveVector.y > 0 && !isRotating && !moveFurther)
+        {
             moveVector.y = 0;
             print("stop");
             //faut rajouter une condition pour que ça avance un peu plus pour activer la condition de la rotation    
-            
+
         }
-        if(raycasts[1].collider == null && !firstTime && moveVector.y < 0 && !isRotating){
-            moveVector.y = 0; 
-        }    
-        
-        // limite movement gauche droite
-        if(raycasts[2].collider == null && !firstTime && moveVector.x > 0 && !isRotating){
-            moveVector.x = 0;          
-        }    
-        if(raycasts[3].collider == null && !firstTime && moveVector.x < 0 && !isRotating){
-            moveVector.x = 0;       
-        }    
+        if (raycasts[1].collider == null && !firstTime && moveVector.y < 0 && !isRotating)
+        {
+            moveVector.y = 0;
+        }
+
         // règle le mouvement forward backward en fonction  de la camera et le mouvement de gauche droite en fonction du player       
-        
-        if(moveVector.x > 0){
+
+        if (moveVector.x > 0)
+        {
             // transform.rotation = Quaternion.Euler(transform.up * 90);
-            transform.Rotate(Vector3.up * 1f);    
+            transform.Rotate(Vector3.up * 1f);
+            // limite movement 
+            if (raycasts[2].collider == null && !firstTime && moveVector.x > 0 && !isRotating)
+            {
+                moveVector.x = 0;
+            }
         }
-        if(moveVector.x < 0){
+        if (moveVector.x < 0)
+        {
             // transform.rotation = Quaternion.Euler(transform.up * 90);
-            transform.Rotate(Vector3.up * -1f);    
+            transform.Rotate(Vector3.up * -1f);
+            if (raycasts[3].collider == null && !firstTime && moveVector.x < 0 && !isRotating)
+            {
+                moveVector.x = 0;
+            }
         }
 
         // movement =  new Vector3(moveVector.x, 0, moveVector.y) * movementSpeed;
         movement = transform.forward * moveVector.y;// + transform.forward * moveVector.x;
         movement *= movementSpeed;
-    
-        if(movement!= Vector3.zero ){ // movement.magnitude.zero => on peut faire ça aussi , magnitude c'est la longueur du vecteur
-            // permet que le perso regarde vers là où il va, le perso se tourne doucement
-            // transform.forward = cameraPlayer2D;
+
+        if (movement != Vector3.zero)
+        { // movement.magnitude.zero => on peut faire ça aussi , magnitude c'est la longueur du vecteur
+          // permet que le perso regarde vers là où il va, le perso se tourne doucement
+          // transform.forward = cameraPlayer2D;
 
             // transform.forward = Vector3.Lerp(transform.forward, movement, 0.1f);
-            
-            controller.Move( movement * Time.deltaTime); //le joueur se déplace Move est une méthode du player controller
+
+            controller.Move(movement * Time.deltaTime); //le joueur se déplace Move est une méthode du player controller
         }
-        
-        print("rotationDown : " + footRotateDown);
+
+
         // print("isRotating : " +isRotating);
-        // if(rayFrontUndertHit.collider != null){
-        //     Vector3 result = rayFrontUndertHit.normal;
-        //     print(result * -1); 
-        // }
+        if(rayFrontUndertHit.collider != null){
+            Vector3 result = rayFrontUndertHit.normal;
+            print(result * -1); 
+        }
     }
 
-    IEnumerator RotationDown(RaycastHit rayFrontUndertHit){
-        transform.forward = rayFrontUndertHit.normal * -1;
+    IEnumerator RotationDown(RaycastHit rayFrontUndertHit)
+    {
+        Vector3 normal = rayFrontUndertHit.normal * -1;
+        transform.forward = normal;
+        Debug.DrawRay(rayFrontUndertHit.transform.position, normal * 100, Color.cyan, 10f);
 
         isRotating = true;
         float time = 0;
-        Quaternion start = transform.localRotation;
-        Quaternion r = Quaternion.Euler(start.eulerAngles + Vector3.right * 90);
+        // Quaternion start = transform.localRotation;
         
+        // Quaternion r = Quaternion.Euler(start.eulerAngles + Vector3.right * 90);
+        Vector3 start = transform.up;
+        Vector3 end = normal;
+
         float ratio = 0;
         Vector3 basePosition = transform.position;
+        transform.up = normal;
 
-
-        while (ratio <1f){
+        while (ratio < 1f && false)
+        {
             transform.position = basePosition;
             time += Time.deltaTime;
             ratio = time / durationRotation;
             // print(ratio);
-            
-            transform.localRotation =  Quaternion.Lerp(start, r, ratio);
+
+            // transform.localRotation =  Quaternion.Lerp(start, end, ratio);
+            Debug.DrawRay(transform.position, Vector3.Lerp(start, end, ratio) * 100, Color.yellow,1f);
+            transform.up = Vector3.Lerp(start, end, ratio);
             //transform.Rotate(Vector3.right * speedRotation * Time.deltaTime, Space.Self);
             //Quaternion angulu = Quaternion.Lerp(start, r, ratio);
             // transform.Rotate(Vector3.right * 90, Space.Self);
-            
+
             yield return true;
             // yield return new WaitForEndOfFrame(); c'est pareil que yield return true
         }
-        
+
         isRotating = false;
-        
+
 
     }
 
