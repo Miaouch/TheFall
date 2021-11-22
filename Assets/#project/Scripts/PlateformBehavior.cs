3ce8140d5d6feb92e33a4f25ba907aef092f0df6
Marie <fzgaoui@hotmail.fr>
2021-11-22 15:02:16 +0100

    avancement correction triggers orientations

--- Assets/#project/Scripts/PlateformBehavior.cs
+++ Assets/#project/Scripts/PlateformBehavior.cs
@@ -11,26 +11,49 @@ public class PlateformBehavior : MonoBehaviour
     public Transform player;
     public Transform pivotsTrigger;
     public int increment = 90;
+    public GameObject cube;
+    public bool isRotating;
+    public float angleRotation;
+    public bool activatedTrigger;
+    
     void Start()
     {
-        
+        activatedTrigger = false;
+        Mesh mesh = cube.GetComponent<MeshFilter>().mesh;
+        Vector3[] normals = mesh.normals;
     }
 
     
     void Update()
     {
-        Vector3 direction = transform.up * -1;
+        isRotating = player.GetComponent<PlayerBehavior>().isRotating;
+
+        Vector3 direction = player.transform.up * -1;
         float hitRange = 1f;
         RaycastHit objectHit;
         Color e = Color.red;
-        if(Physics.Raycast(transform.position, direction, out objectHit, hitRange)){
+        if(Physics.Raycast(player.transform.position, direction, out objectHit, hitRange)){
             e = Color.green; }
-        Debug.DrawRay(transform.position, direction * hitRange, e);
+        Debug.DrawRay(player.transform.position, direction * hitRange, e);
+        if(objectHit.transform.gameObject == cube && !isRotating){
+            print("prout");
+            activatedTrigger = true;
+        } else{
+            activatedTrigger = false;
+            
+        }
+        
+        pivotsTrigger.gameObject.SetActive(activatedTrigger);
+        
 
+        
+        angleRotation = Vector3.SignedAngle(transform.forward, player.forward, transform.up);
+        
+        pivotsTrigger.localEulerAngles = new Vector3(0, (Mathf.Round(angleRotation / increment) * increment), 0);
+        
+        print($"localRotation {player.localRotation.eulerAngles}");
+        
 
-        // pivotsTrigger.rotation = player.rotation;
-        pivotsTrigger.eulerAngles = new Vector3(player.rotation.eulerAngles.x, (Mathf.Round(player.rotation.eulerAngles.y / increment) * increment), player.rotation.eulerAngles.z);
-        if(objectHit.normal = Vector3.up);
     }
 
     
